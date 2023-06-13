bb609bb22dcb9fec47bdf5f1408d152fcb58c72e
Jevis Xandra <111032186+1BeginnerJun1@users.noreply.github.com>
2023-06-13 22:42:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ var scriptElement = document.createElement('script');
 scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
 vizElement.parentNode.insertBefore(scriptElement, vizElement);
 </script>
-""", height = 1200)
+""", height = 1250)
 
 st.markdown(" ")
 g10, g11, g12 = st.columns([1,13,1])
